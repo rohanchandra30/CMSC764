a243e1d10f1659ed8ea83ff2539d3e80c737f961
rohanchandra30 <rohanchandra30@gmail.com>
2017-03-22 20:12:15 -0400

    renaming hmwk1-4

--- hmwk4_sols_matlab/hmwk2/create_classification_problem.m
+++
@@ -1,9 +0,0 @@
-function [ D,c ] = create_classification_problem(Nd, Nf, kappa)
-D = buildmat(Nd,Nf,kappa);
-w = randn(Nf,1);
-c = sign(D*w);
-%  The the problem not quite separable by flipping some signs.
-flipper = ones(Nd,1) - 2*(rand(Nd,1)>.9);
-c = c.*flipper;
-end
-
--- hmwk4_sols_matlab/hmwk3/tv_grad.m
+++
@@ -1,8 +0,0 @@
-function grad = tv_grad( x, b, mu, eps )
-grad = mu*div2d(l1_grad(grad2d(x),eps)) + x-b;
-return
-
-function rval = l1_grad(x, eps)
-rval = x./sqrt(x.*x+eps*eps);
-return
-
